9aa669958579048dcabbfb9e8e9194ded9a26e9d
Shaun Lim <slim0125@student.monash.edu>
2024-09-18 19:09:17 +0800

    new thingsss

--- W7/Part B - Intro to vega-lite/1. basic bar chart/vega_lite_vis.js
+++ W7/Part B - Intro to vega-lite/1. basic bar chart/vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "daily_cost_bar_chart.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "weather_chart.vg.json";
+vegaEmbed("#bar_weather_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
